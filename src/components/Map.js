76f48629ee7484d7c6c021ae3ee9c7368514a279
Qpx Design <71609644+QpxDesign@users.noreply.github.com>
2023-01-07 09:56:46 -0500

    Update Map.js

--- src/components/Map.js
+++ src/components/Map.js
@@ -302,7 +302,6 @@ export default function Map() {
       } • Arriving At ${helper(r.tripUpdate.stopTimeUpdate, stop_id)}</h2>`;
     });
     if (results_display !== "") {
-      console.log(results_display);
       return results_display
         .replaceAll("Train", "Train ")
         .replaceAll("Saturday", " Saturday")
